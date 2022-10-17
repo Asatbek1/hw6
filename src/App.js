76a63b45afeefebe27fe00ad7c1140ad85a69921
Asatbek <asatbekisabekuulu@gmail.com>
2022-10-17 15:02:12 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCound] = useState(0);
+  const [toggleBtn, setToggleBtn] = useState(false);
+  function inc() {
+    setCound((prev) => prev + 1);
+  }
+  function dec() {
+    setCound((prev) => prev - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h3>Count:{count}</h3>
+        <button onClick={inc}>INC</button>
+        <button onClick={dec}>DEC</button>
+<button onClick={()=> setToggleBtn(!toggleBtn)}>Toggle btn</button>
+        {toggleBtn ? <p className="lead">YouTube Content</p> : null}
+      </div>
+    </>
   );
 }
 
